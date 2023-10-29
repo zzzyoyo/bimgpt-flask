e138c7a586bdcaf6fe113378dbf4c5a91acc0027
zzzyoyo <2281744227@qq.com>
2023-10-29 23:18:44 +0800

    基本页面

--- home.py
+++ home.py
@@ -1,11 +1,25 @@
+import json
+
 from flask import Flask, render_template, request, jsonify
 from segment_and_classify import segment, classify
+from bracket_structure import chatgpt_build_bracket
+
 app = Flask(__name__)
+dictionary = None
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/uploadDict', methods=['POST'])
+def upload_dict():
+    file_content = request.files['file'].read().decode('utf-8')
+    global dictionary
+    dictionary = json.loads(file_content)
+    # print(dictionary)
+    return 'success'
 
 
 @app.route('/classify', methods=['GET', 'POST'])
@@ -16,13 +30,23 @@ def seg_classify():
         if uploaded_file.filename != '':
             # 处理上传的文件内容
             file_content = uploaded_file.read().decode('utf-8')
-            seg_list = segment(file_content)
-            segmented_text = [word for word, _ in seg_list]
-
-            classified_dict = classify(seg_list)
+            resps = json.loads(file_content)
+            seg_list = segment(resps)
+            word_list = []
+            for item in seg_list:
+                word_list += item['words']
+                item['words'] = ' | '.join(item['words'])
+            word_list = list(set(word_list))
+            # print(word_list)
+            if dictionary is None:
+                classified_dict = "没有上传字典，无法进行分类"
+            else:
+                classified_dict = classify(dictionary, word_list)
+            # print(classified_dict)
 
             # 返回两个结果
-            return jsonify({'classification_result': classified_dict, 'segmentation_result': '\n'.join(segmented_text)})
+            return jsonify({'classification_result': json.dumps(classified_dict, ensure_ascii=False, indent=4),
+                            'segmentation_result': json.dumps(seg_list, ensure_ascii=False, indent=4)})
 
     return render_template('classify.html')
 
@@ -37,8 +61,11 @@ def extract():
             file_content = uploaded_file.read().decode('utf-8')
 
             # 调用句子结构提取函数处理文本
+            text_list = file_content.split('\n')
+            text_list = [text.strip() for text in text_list]
+            print(text_list)
 
-            return file_content
+            return chatgpt_build_bracket(text_list)
 
     return render_template('structure.html')
 
@@ -51,7 +78,6 @@ def generate():
         if uploaded_file.filename != '':
             # 处理上传的文件内容
             file_content = uploaded_file.read().decode('utf-8')
-
 
             # 设置metric和pass rate的值
             metric = 0.85  # 举例，根据实际情况设置metric的值
--- segment_and_classify.py
+++ segment_and_classify.py
@@ -7,6 +7,7 @@ from torch.utils.data import DataLoader, Dataset
 from transformers import BertTokenizer, BertModel
 from torch import cuda
 import json
+import re
 import os
 
 device = 'cuda' if cuda.is_available() else 'cpu'
@@ -110,8 +111,45 @@ class SingleLabelBERTModel(nn.Module):
         return linear_output
 
 
-def segment(file_content: str):
-    return pseg.cut(file_content)
+def refine_words(word):
+    refined_words = []
+    word_list = jieba.lcut(word)
+    new_word = ""
+    for w in word_list:
+        if w in ["等", "的", "以", "为", "为了", "由", "在", "对", "对于",  "和", "、", "与", "及", "以及", "及其", "如果", "那么", "否则", "且", "并且", "并", "或", "或者", "应", "不应", "应该", "应当", "必须", "宜", "不宜", "可", "不", "不可", "不得", "严禁", "等于", "不等于", "大于", "不大于", "小于", "不小于", "高于", "不高于", "低于", "不低于",
+            "至少", "至多", "少于", "不少于", "多于", "不多于",
+            "+", "-", "*", "/", ">", "<", ">=", "<=", "=", "!=", "有", "具有", "设置", "设有", "提供", "纳入",  "满足", "符合", "遵循", "依据", "根据", "按", "按照"]:
+            if len(new_word) > 0:
+                refined_words.append(new_word)
+            refined_words.append(w)
+            new_word = ""
+        else:
+            new_word += w
+    if len(new_word) > 0:
+        refined_words.append(new_word)
+    return refined_words
+
+
+
+def segment(resps):
+    for pair in resps:
+        resp = pair['response'].strip()
+        start = 0
+        current = 0
+        words = []
+        while current < len(resp):
+            if resp[current] in ['\n', '\t', '\r', ' ', '，', '。', '；', '-', '、', '(', ')', '（', '）']:
+                if current > start:
+                    words += refine_words(resp[start:current])
+                current += 1
+                start = current
+            else:
+                current += 1
+        if current > start:
+            words += refine_words(resp[start:current])
+        pair['words'] = words
+    # print(resps)
+    return resps
 
 
 def single_label_validation(model, testing_loader):
@@ -147,7 +185,7 @@ def multi_label_validation(model, testing_loader):
     return fin_outputs, fin_targets
 
 
-def classify(words):
+def classify(dictionary, words):
     results_map = {
         "NounItems": {},
         "VerbItems": {},
@@ -155,15 +193,47 @@ def classify(words):
         "OtherItems": {}
     }
     test_texts = []
-    for word, flag in words:
+    for word in words:
+        # 先从字典里找
+        if word in dictionary["NounItems"]:
+            results_map["NounItems"][word] = dictionary["NounItems"][word]
+            continue
+        elif word in dictionary["VerbItems"]:
+            results_map["VerbItems"][word] = dictionary["VerbItems"][word]
+            continue
+        elif word in dictionary["AdItems"]:
+            results_map["AdItems"][word] = dictionary["AdItems"][word]
+            continue
+        elif word in dictionary["OtherItems"]:
+            results_map["OtherItems"][word] = dictionary["OtherItems"][word]
+            continue
+        # 再用jieba分词来分roottag
+        jieba.add_word(word)
+        pseg_result = pseg.lcut(word)
+        assert len(pseg_result) == 1
+        word, flag = pseg_result[0]
+        # print(word, '|', flag)
         if 'v' in flag:
-            results_map["VerbItems"][word] = {"flag": flag}
+            results_map["VerbItems"][word] = {
+                "Name": word,
+                "RootTag": "VERB",
+                "SecondaryTag": flag,
+                "FineTags": [],
+                "DataMapping": None
+            }
         elif 'a' in flag or 'd' in flag:
-            results_map["AdItems"][word] = {"flag": flag}
+            results_map["AdItems"][word] = {
+                "Name": word,
+                "RootTag": "AD",
+                "SecondaryTag": flag,
+                "FineTags": [],
+                "DataMapping": None
+            }
         else:
             # 都拿去分类
             test_texts.append(word)
 
+    print(test_texts)
     tokenizer = BertTokenizer.from_pretrained(r'D:\bimgpt\d40_rulegeneration\Coarse&Fine-grained Classification\bert-chinese-base')
     test_labels = [0 for i in range(len(test_texts))]  # 没有标签的测试集，随便生成一个长度和texts一样的即可
     testing_set = MultiClassDataset(test_texts, test_labels, tokenizer, MAX_LEN)
@@ -199,7 +269,7 @@ def classify(words):
         2: "PEOPLE",
         3: "OTHER"
     }
-    with open('dataset/threshold=50,level=0/IndexToLabels.txt', 'r', encoding='utf-8') as f:
+    with open(r'D:\bimgpt\d40_rulegeneration\Coarse&Fine-grained Classification\dataset\threshold=50,level=0\IndexToLabels.txt', 'r', encoding='utf-8') as f:
         labels_name = [line[line.index(':') + 1:].replace('\n', '') for line in f.readlines()]
     for i in range(len(test_texts)):
         if coarse_outputs[i] != 0:
